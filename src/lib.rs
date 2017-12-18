0219f96296f2f44d8cbd761e91b1ef05c195af3e
Mack Stump <mack.stump@gmail.com>
2017-12-18 15:54:16 -0500

    add hello world in wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[no_mangle]
 pub fn example() -> i32 {
     42
+}
+
+#[no_mangle]
+pub fn hello() -> *mut c_char {
+    CString::new("Hello, world!").unwrap().into_raw()
 }
