d715bc459dd5aa1ec8e313bdfec37c01b1b43f22
Mack Stump <mack.stump@gmail.com>
2017-12-18 16:31:56 -0500

    add calling javascript from wasm example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::ffi::CString;
 use std::os::raw::c_char;
 
+extern {
+    fn alert(ptr: *const c_char);
+}
+
 #[no_mangle]
 pub fn example() -> i32 {
     42
@@ -9,4 +13,12 @@ pub fn example() -> i32 {
 #[no_mangle]
 pub fn hello() -> *mut c_char {
     CString::new("Hello, world!").unwrap().into_raw()
+}
+
+#[no_mangle]
+pub fn call_alert() {
+    unsafe {
+        let s = CString::new("Hello, world!").unwrap();
+        alert(s.as_ptr());
+    }
 }
