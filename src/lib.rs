fcdd1b6e29229098b7ba0ccd1f5589a4f54d3dd9
Mack Stump <mack.stump@gmail.com>
2017-12-19 12:00:05 -0500

    add example using incrementing global counter

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::ffi::CString;
 use std::os::raw::c_char;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 mod js;
+
+lazy_static! {
+    static ref CLICK_COUNT: AtomicUsize = AtomicUsize::new(1);
+}
 
 #[no_mangle]
 pub fn example() -> i32 {
@@ -17,4 +25,12 @@ pub fn hello() -> *mut c_char {
 pub fn call_js() {
     js::alert("Hello, world!");
     js::log("Hello, world!");
+}
+
+#[no_mangle]
+pub fn clickme_clicked() {
+    let count = CLICK_COUNT.fetch_add(1, Ordering::Relaxed);
+    js::log(&format!("Clicked {} time{}",
+                     count,
+                     if count == 1 { "" } else { "s" }));
 }
