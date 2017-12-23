592c1d4403ade5452a764fd449995aed21245e94
Mack Stump <mack.stump@gmail.com>
2017-12-22 22:58:04 -0500

    optimize canvas example

--- src/lib.rs
+++ src/lib.rs
@@ -47,34 +47,24 @@ pub extern "C" fn alloc(size: usize) -> *mut c_void {
 }
 
 #[no_mangle]
-pub fn fill(pointer: *mut u8, max_width: usize, max_height: usize, time: f64) {
-
+pub fn fill(pointer: *mut u32, max_width: usize, max_height: usize, time: f64) {
     // pixels are stored in RGBA, so each pixel is 4 bytes
-    let byte_size = max_width * max_height * 4;
-    let sl = unsafe { slice::from_raw_parts_mut(pointer, byte_size) };
+    let sl = unsafe { slice::from_raw_parts_mut(pointer, max_width*max_height) };
 
-    for i in 0..byte_size {
-
+    for i in 0..max_width*max_height {
         // get the position of current pixel
-        let height = i / 4 / max_width;
-        let width  = i / 4 % max_width;
+        let height = i / max_width;
+        let width  = i % max_width;
 
-        if i%4 == 3 {
-            // set opacity to 1
-            sl[i] = 255;
-        } else if i%4 == 0 {
-            // create a red ripple effect from the top left corner
-            let len = ((height*height + width*width) as f64).sqrt();
-            let nb = time  + len / 4.0;
-            let a = 128.0 + nb.cos() * 128.0;
-            sl[i] = a as u8;
-        } else if i % 4 == 2 {
-            // create a blue ripple effect from the top right corner
-            let width = 500 - width;
-            let len = ((height*height + width*width) as f64).sqrt();
-            let nb = time  + len / 4.0;
-            let a = 128.0 + nb.cos() * 128.0;
-            sl[i] = a as u8;
-        }
+        let len = ((height*height + width*width) as f64).sqrt();
+        let nb = time  + len / 4.0;
+        let r = 128.0 + nb.cos() * 128.0;
+
+        let width = 500 - width;
+        let len = ((height*height + width*width) as f64).sqrt();
+        let nb = time  + len / 4.0;
+        let b = 128.0 + nb.cos() * 128.0;
+
+        sl[i] = 0xff << 24 | (b as u8 as u32) << 16 | 0x00 << 8 | (r as u8 as u32);
     }
 }
